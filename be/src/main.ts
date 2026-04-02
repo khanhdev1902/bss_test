b56fed4c6653c2dbfa0b9c8817a44cdb2ba3a2cc
Văn Khanh <khanhdev1902@gmail.com>
2026-04-02 11:13:00 +0700

    add link fe

--- be/src/main.ts
+++ be/src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', ' http://localhost:4173/'],
+    origin: [
+      'http://localhost:5173',
+      ' http://localhost:4173/',
+      'https://bss-test.vercel.app',
+    ],
   });
   await app.listen(process.env.PORT ?? 3000);
 }
